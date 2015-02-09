e1de48e73a83a3a1d6a5e0633dcb80acfbae1e71
lebebr01 <lebebr01@gmail.com>
2015-02-09 12:11:29 -0600

    fix error in sim.fact

--- R/fixefSim.r
+++ R/fixefSim.r
@@ -58,7 +58,7 @@ sim.fixef.nested <- function(fixed, fixed.vars, cov.param, n, p, data.str,
   
   if(length(fact.loc > 0)){
     fact.vars <- lapply(1:length(fact.vars), function(xx) 
-      list(n = n, p = p, fact.vars[[xx]]))
+      list(k = 0, n = n, p = p, fact.vars[[xx]]))
     Xmat <- cbind(Xmat, do.call("cbind", lapply(1:n.fact, 
               function(xx) do.call(sim.factor, fact.vars[[xx]]))))
   }
@@ -190,7 +190,8 @@ sim.fixef.single <- function(fixed, fixed.vars, n, cov.param, fact.vars = list(N
     do.call(sim.continuous, cov.param[[xx]])))
   
   if(length(fact.loc > 0)){
-    fact.vars <- lapply(1:length(fact.vars), function(xx) list(n = n, fact.vars[[xx]]))
+    fact.vars <- lapply(1:length(fact.vars), function(xx) 
+      list(k = 0, n = n, p = 0,, fact.vars[[xx]]))
     Xmat <- cbind(Xmat, do.call("cbind", lapply(1:n.fact, 
             function(xx) do.call(sim.factor, fact.vars[[xx]]))))
   }

58e1624ddd231a2094cd5ed24b221da80a00dfcb
lebebr01 <lebebr01@gmail.com>
2015-06-24 12:07:27 -0500

    Update examples to reflect changes to err_sim

--- R/sim_reg.r
+++ R/sim_reg.r
@@ -22,15 +22,13 @@
 #' @param k Number of third level clusters.
 #' @param n Cluster sample size.
 #' @param p Within cluster sample size.
-#' @param errorVar Scalar of error variance.
+#' @param error_var Scalar of error variance.
 #' @param randCor Correlation between random effects.
 #' @param randCor3 Correlation between third level random effects.
 #' @param rand.dist Simulated random effect distribution.  Must be "lap", "chi", "norm", "bimod", 
 #' "norm" is default.
-#' @param err.dist Simulated within cluster error distribution. Must be "lap", "chi", "norm", "bimod", 
-#' "norm" is default.
-#' @param serCor Simulation of serial correlation. Must be "AR", "MA", "ARMA", or "ID", "ID" is default.
-#' @param serCorVal Serial correlation parameters. A list of values to pass on to arima.sim.
+#' @param rand_gen Distribution function to pass on to the level one
+#'                  simulation of errors.
 #' @param data.str Type of data. Must be "cross", "long", or "single".
 #' @param fact.vars A nested list of factor, categorical, or ordinal variable specification, 
 #'      each list must include numlevels and var.type (must be "lvl1" or "lvl2");
@@ -45,6 +43,7 @@
 #'  will be drawn from a random uniform distribution with min and max specified.
 #' @param unbalCont3 When unbal3 = TRUE, this specifies the minimum and maximum level two size,
 #'  will be drawn from a random uniform distribution with min and max specified.
+#'  @param ... Additional parameters passed on to the level one error generating function
 #' @export 
 #' @examples
 #' \donttest{
@@ -53,10 +52,10 @@
 #' fixed.param <- c(2, 4, 1, 3.5, 2)
 #' cov.param <- list(mean = c(0, 0, 0), sd = c(4, 3, 3), var.type = c("single", "single", "single"))
 #' n <- 150
-#' errorVar <- 3
-#' err.dist <- "norm"
+#' error_var <- 3
+#' rand_gen <- rnorm
 #' temp.single <- sim.reg(fixed = fixed, fixed.param = fixed.param, cov.param = cov.param, 
-#' n = n, errorVar = errorVar, err.dist = err.dist, data.str = "single")
+#' n = n, error_var = error_var, rand_gen = rand_gen, data.str = "single")
 #' # Fitting regression to obtain parameter estimates
 #' summary(lm(sim.data ~ 1 + act + diff + numCourse + act:numCourse, data = temp.single))
 #' 
@@ -68,15 +67,13 @@
 #' cov.param <- list(mean = c(0, 0), sd = c(1.5, 4), var.type = c("lvl1", "lvl2"))
 #' n <- 150
 #' p <- 30
-#' errorVar <- 4
+#' error_var <- 4
 #' randCor <- 0
 #' rand.dist <- "norm"
-#' err.dist <- "norm"
-#' serCor <- "ID"
-#' serCorVal <- NULL
+#' rand_gen <- rnorm
 #' data.str <- "long"
 #' temp.long <- sim.reg(fixed, random, random3 = NULL, fixed.param, random.param, random.param3 = NULL,
-#'  cov.param, k = NULL, n, p, errorVar, randCor, randCor3 = NULL, rand.dist, err.dist, serCor, serCorVal, 
+#'  cov.param, k = NULL, n, p, error_var, randCor, randCor3 = NULL, rand.dist, rand_gen, 
 #'  data.str)
 #' 
 #' ## fitting lmer model
@@ -96,16 +93,14 @@
 #' k <- 10
 #' n <- 150
 #' p <- 30
-#' errorVar <- 4
+#' error_var <- 4
 #' randCor <- 0
 #' randCor3 <- 0
 #' rand.dist <- "norm"
-#' err.dist <- "norm"
-#' serCor <- "ID"
-#' serCorVal <- NULL
+#' rand_gen <- rnorm
 #' data.str <- "long"
 #' temp.three <- sim.reg(fixed, random, random3, fixed.param, random.param, random.param3, cov.param, k,
-#' n, p, errorVar, randCor, randCor3, rand.dist, err.dist, serCor, serCorVal, data.str)
+#' n, p, error_var, randCor, randCor3, rand.dist, rand_gen, data.str)
 #' 
 #' library(lme4)
 #' lmer(sim.data ~ 1 + time + diff + act + actClust + time:act + (1 + time + diff | clustID) +  
@@ -113,21 +108,21 @@
 #' 
 #' }
 sim_reg <- function(fixed, random, random3, fixed.param, random.param, random.param3, cov.param, k, n, p, 
-                    errorVar, randCor, randCor3, rand.dist, err.dist, 
-                    serCor, serCorVal, data.str, fact.vars = list(NULL),
+                    error_var, randCor, randCor3, rand.dist, rand_gen, 
+                    data.str, fact.vars = list(NULL),
                     unbal = FALSE, unbal3 = FALSE, unbalCont = NULL, unbalCont3 = NULL) {
   
   if(data.str == "single"){
-    sim_reg_single(fixed, fixed.param, cov.param, n, errorVar, err.dist, data.str, fact.vars)
+    sim_reg_single(fixed, fixed.param, cov.param, n, error_var, rand_gen, data.str, fact.vars)
   } else {
   	if (is.null(k)){
   	  sim_reg_nested(fixed, random, fixed.param, random.param, cov.param, n, p, 
-  	                 errorVar, randCor, rand.dist, err.dist, serCor, 
+  	                 error_var, randCor, rand.dist, rand_gen, serCor, 
   	                 serCorVal, data.str, fact.vars,
   	                 unbal, unbalCont)
   } else {
     sim_reg_nested3(fixed, random, random3, fixed.param, random.param, random.param3, cov.param, k, n, p, 
-                    errorVar, randCor, randCor3, rand.dist, err.dist, 
+                    error_var, randCor, randCor3, rand.dist, rand_gen, 
                     serCor, serCorVal, data.str, fact.vars,
                     unbal, unbal3, unbalCont, unbalCont3)
   }

0673ca5f53b079d99ea8b6f461fc9d191c6ced59
lebebr01 <lebebr01@gmail.com>
2015-06-24 11:33:14 -0500

    Generalize Level 1 Error simulation

--- R/errSim.r
+++ R/errSim.r
@@ -2,60 +2,28 @@
 #' 
 #' Input error simulation parameters and outputs simulated errors.
 #' 
-#' @param errorVar Scalar of error variance
+#' @param error_var Scalar of error variance
 #' @param n Cluster sample size.
 #' @param p Within cluster sample size.
-#' @param serCor Simulation of serial correlation. Must be "AR", "MA", "ARMA", or "ID", "ID" is default.
-#' @param serCorVal Serial correlation parameters. A list of values to pass on to arima.sim.
-#' @param err.dist Simulated within cluster error distribution. Must be "lap", "chi", "norm", 
-#' "norm" is default.
-#' @importFrom VGAM rlaplace 
-#' @importFrom MASS mvrnorm
+#' @param rand_gen The generating function used.
+#' @param arima TRUE/FALSE flag indicating whether residuals should 
+#'             be correlated. If TRUE, must specify a valid model to pass to 
+#'             arima.sim, \seealso{arima.sim}
+#' @param ... Additional specification needed to pass to the random generating 
+#'             function defined by rand.gen.
 #' @export 
-sim.err.nested <- function(errorVar, n, p, serCor, serCorVal, err.dist){
+sim.err.nested <- function(error_var, n, p, rand_gen, arima = FALSE, ...){
   
   # Look to edit this with match.arg and switch
   #n <- length(p)
 
-  if(serCor == "ARMA" & length(serCorVal) < 2) stop("Incorrect dimensions serCorVal")
-  if(err.dist == "norm"){
-    if(serCor == "AR"){
-      err <- unlist(lapply(1:n, function(x) {
-        arima.sim(serCorVal, p[x], sd = sqrt(errorVar))
-        }))
-    } else {
-      if(serCor == "MA"){
-        err <- unlist(lapply(1:n, function(x) {
-          arima.sim(serCorVal, p[x], sd = sqrt(errorVar))
-          }))
-      } else {
-        if(serCor == "ARMA"){
-          err <- unlist(lapply(1:n, function(x) {
-            arima.sim(serCorVal, p[x], sd = sqrt(errorVar))
-            }))
-        } else {
-          # generate multivariate normal error terms with zero mean 
-          #d2 <- (errorVar)*diag(p) 
-          err <- unlist(lapply(1:length(p), function(xx) 
-            mvrnorm(n = 1,rep(0,p[xx]), Sigma = (errorVar) * diag(p[xx]))))
-        }
-      }
-    }
+  if(arima) {
+    err <- unlist(lapply(lapply(1:n, function(xx) 
+      arima.sim(n = p, rand.gen = rand_gen, ...)), scale)) * sqrt(error_var)
+  } else {
+    err <- unlist(lapply(lapply(1:n, rand_gen, n = p, ...), scale)) * sqrt(error_var)
   }
-  
-  if(err.dist == "lap"){
-    err <- unlist(lapply(1:n, function(x){rlaplace(p,0,1)*chol(errorVar/2)}))
-  }
-  
-  if(err.dist == "chi"){
-    err <- unlist(lapply(1:n, function(x){(rchisq(p,1)-1)*chol(errorVar/2)}))
-  }
-  
-#   if(err.dist == "bimod"){
-#     err <- unlist(lapply(1:n, function(x) {
-#       rbimod(p, mean, var, num.dist) }))
-#   }
-err
+  return(err)
 }
 
 
@@ -65,32 +33,22 @@ err
 #' 
 #' Simulates error term for single level regression models.
 #' 
-#' @param errorVar Scalar of error variance
+#' @param error_var Numeric scalar of error variance
 #' @param n Cluster sample size.
-#' @param err.dist Simulated within cluster error distribution. Must be "lap", "chi", "norm", 
-#' "norm" is default.
-#' @param rand.gen The generating function used with arima.sim.
+#' @param rand_gen The generating function used.
+#' @param arima TRUE/FALSE flag indicating whether residuals should 
+#'             be correlated. If TRUE, must specify a valid model to pass to 
+#'             arima.sim, \seealso{arima.sim}.
 #' @param ... Additional values that need to be passed to the function
 #'             called from rand.gen.
 #' @importFrom VGAM rlaplace
 #' @export 
-sim.err.single <- function(errorVar, n, err.dist, rand.gen = NULL, ...){
+sim.err.single <- function(error_var, n, rand_gen, arima = FALSE, ...){
   
-  if(is.null(rand.gen) == FALSE) {
-    err <- arima.sim(n = n, rand.gen = rand.gen, ...)
+  if(arima) {
+    err <- scale(arima.sim(n = n, rand.gen = rand_gen, ...)) * sqrt(error_var)
   } else {
-    if(err.dist == "norm"){
-      err <- rnorm(n, 0, sd = sqrt(errorVar))
-    }
-    if(err.dist == "lap"){
-      err <- rlaplace(n,0,1)*chol(errorVar/2)
-    }
-    if(err.dist == "chi"){
-      err <- (rchisq(n,1)-1)*chol(errorVar/2)
-    }
-    #   if(err.dist == "bimod"){
-    #     err <- rbimod(n, mean, var, num.dist)
-    #   }
+    err <- scale(sapply(X = n, FUN = rand_gen, ...)) * sqrt(error_var)
   }
-  err
+  return(err)
 }

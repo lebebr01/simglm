ec7adbb6cae1593c462c7059acf3e37e20106651
lebebr01 <lebebr01@gmail.com>
2015-06-24 11:38:53 -0500

    rename all files from nameName to name_name

---
+++ R/varcov_randeff.r
@@ -0,0 +1,27 @@
+#' Function to create random effect variance-covariance matrices
+#' 
+#' Input variances of random effects and correlation between random effects,
+#' returns variance-covariance matrix of random effects.
+#' 
+#' @param random.param Variance of random effects.
+#' @param cor Correlation between random effects, currently only a constant supported.
+#' @export 
+varcov.randeff <- function(random.param, cor){
+  
+  if (any(random.param < 0)) 
+    stop("All 'random.param' should be positive")
+  if(cor == 0 | length(random.param == 1)) {
+    mat <- diag(random.param, nrow = length(random.param), ncol = length(random.param))
+  } else {
+    mat <- diag(1, nrow = length(random.param), ncol = length(random.param))
+    delta <- row(mat) - col(mat)
+    mat[delta != 0] <- cor
+    
+    sd <- sqrt(random.param)
+    n <- sqrt(length(mat))
+    if (n != length(sd)) 
+      stop("The length of 'random.param' should be the same as the number of rows of 'mat'")
+    mat <- diag(sd) %*% mat %*% diag(sd)  
+  }
+ return(mat)
+}

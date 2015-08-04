04bb4c85f0c7dd094a993f58eebf933402b69e2e
lebebr01 <lebebr01@gmail.com>
2015-08-04 12:23:39 -0500

    Three level logistic regression simulation.

--- R/data_glm.r
+++ R/data_glm.r
@@ -33,6 +33,7 @@ data_glm_single <- function(Xmat, beta, n) {
 #' @param rand.eff A vector of random effects, must be stacked.
 #' @param n Number of clusters.
 #' @param p Number of units within each cluster.
+#' @importFrom Matrix bdiag
 #' @export
 data_glm_nested <- function(Xmat, Zmat, beta, rand.eff, n, p) {
   
@@ -51,5 +52,57 @@ data_glm_nested <- function(Xmat, Zmat, beta, rand.eff, n, p) {
   sim_data <- rbinom(length(prob), 1, prob)
   sim.data <- cbind(Fbeta, re, logistic, prob, sim_data)
   colnames(sim.data) <- c("Fbeta", "randEff", 'logistic', 'prob', "sim.data")
+  return(sim.data)
+}
+
+#' Simulates three level nested data with a single third level random effect
+#' 
+#' Takes simulation parameter arguments and 
+#' returns simulated data in the logistic metric and
+#' converted to 0/1 based on the probabilities and the 
+#' binomial distribution with 1 trial.
+#' 
+#' @param Xmat A matrix of covariates.
+#' @param Zmat Design matrix for random effects.
+#' @param Zmat3 Design matrix for level 3 random effects.
+#' @param beta A vector of regression parameters.
+#' @param rand.eff A vector of random effects, must be stacked.
+#' @param rand.eff3 A vector of level 3 random effects, must be stacked.
+#' @param k Number of third level clusters.
+#' @param n Number of clusters.
+#' @param p Number of units within each cluster.
+#' @importFrom Matrix bdiag
+#' @export 
+data_glm_nested3 <- function(Xmat, Zmat, Zmat3, beta, rand.eff, rand.eff3, k, n, p) {
+  
+  
+  end <- cumsum(n)
+  beg <- c(1, cumsum(n) + 1)
+  beg <- beg[-length(beg)]
+  
+  lvl3ss <- sapply(lapply(1:length(beg), function(xx) 
+    p[beg[xx]:end[xx]]), sum)
+  
+  Fbeta <- (Xmat %*% beta) 
+  
+  ID <- NULL
+  Zmat <- data.frame(Zmat, ID = rep(1:length(p), times = p))
+  ZmatList <- lapply(1:length(p), function(xx) as.matrix(subset(Zmat, ID == xx, select = 1:(ncol(Zmat) - 1))))
+  ZmatBlock <- bdiag(ZmatList)
+  reVec <- matrix(c(t(rand.eff)))
+  re <- as.matrix(ZmatBlock %*% reVec)
+  
+  Zmat3 <- data.frame(Zmat3, ID = rep(1:k, times = lvl3ss))
+  Zmat3List <- lapply(1:k, function(xx) as.matrix(subset(Zmat3, ID == xx, select = 1:(ncol(Zmat3) - 1))))
+  Zmat3Block <- bdiag(Zmat3List)
+  re3Vec <- as.matrix(c(t(rand.eff3)))
+  re3 <- as.matrix(Zmat3Block %*% re3Vec)
+  
+  logistic <- Fbeta + re + re3
+  prob <- exp(logistic)/(1 + exp(logistic))
+  sim_data <- rbinom(length(prob), 1, prob)
+  sim.data <- cbind(Fbeta, re, re3, logistic, prob, sim_data)
+  colnames(sim.data) <- c("Fbeta", "randEff", 'randEff3', 'logistic', 
+                          'prob', "sim.data")
   return(sim.data)
 }
--- R/sim_glm_func.r
+++ R/sim_glm_func.r
@@ -131,3 +131,139 @@ sim_glm_nested <- function(fixed, random, fixed.param, random.param, cov.param, 
   
 }
 
+#' Function to simulate three level nested data
+#' 
+#' Takes simulation parameters as inputs and returns simulated data.
+#' 
+#' Simulates data for the linear mixed model, both cross sectional and longitudinal data.  Returns
+#' a data frame with ID variables, fixed effects, and many other variables useful to help when 
+#' running simulation studies.
+#' 
+#' @seealso \code{\link{sim_reg}} for a convenient wrapper for all data conditions.
+#' 
+#' @param fixed One sided formula for fixed effects in the simulation.  To suppress intercept add -1 to formula.
+#' @param random One sided formula for random effects in the simulation. Must be a subset of fixed.
+#' @param random3 One sided formula for random effects at third level in the simulation. Must be a subset of fixed
+#'  (and likely of random).
+#' @param fixed.param Fixed effect parameter values (i.e. beta weights).  Must be same length as fixed.
+#' @param random.param Variance of random effects. Must be same length as random.
+#' @param random.param3 Variance of level 3 random effects. Must be same length as random3.
+#' @param cov.param List of mean, sd (standard deviations), and var.type for fixed effects. 
+#'  Does not include intercept, time, factors, or interactions. 
+#'  var.type must be either "lvl1" or "lvl2". Must be same order as fixed formula above.
+#' @param k Number of third level clusters.
+#' @param n Cluster sample size.
+#' @param p Within cluster sample size.
+#' @param randCor Correlation between random effects.
+#' @param randCor3 Correlation between level 3 random effects.
+#' @param rand_dist Simulated random effect distribution.  Must be "lap", "chi", "norm", "bimod", 
+#' "norm" is default.
+#' @param data_str Type of data. Must be "cross", "long", or "single".
+#' @param fact.vars A nested list of factor, categorical, or ordinal variable specification, 
+#'      each list must include numlevels and var.type (must be "lvl1" or "lvl2");
+#'      optional specifications are: replace, prob, value.labels.
+#' @param unbal A vector of sample sizes for the number of observations for each level 2
+#'  cluster. Must have same length as level two sample size n. Alternative specification
+#'  can be TRUE, which uses additional argument, unbalCont.
+#' @param unbal3 A vector of sample sizes for the number of observations for each level 3
+#'  cluster. Must have same length as level two sample size k. Alternative specification
+#'  can be TRUE, which uses additional argument, unbalCont3.
+#' @param unbalCont When unbal = TRUE, this specifies the minimum and maximum level one size,
+#'  will be drawn from a random uniform distribution with min and max specified.
+#' @param unbalCont3 When unbal3 = TRUE, this specifies the minimum and maximum level two size,
+#'  will be drawn from a random uniform distribution with min and max specified.
+#' @param ... Additional specification needed to pass to the random generating 
+#'             function defined by rand.gen.
+#' @examples 
+#' \donttest{
+#' # Three level example
+#' fixed <- ~1 + time + diff + act + actClust + time:act
+#' random <- ~1 + time + diff
+#' random3 <- ~ 1 + time
+#' fixed.param <- c(4, 2, 6, 2.3, 7, 0)
+#' random.param <- c(7, 4, 2)
+#' random.param3 <- c(4, 2)
+#' cov.param <- list(mean = c(0, 0, 0), sd = c(1.5, 4, 2),
+#' var.type = c("lvl1", "lvl2", "lvl3"))
+#' k <- 10
+#' n <- 150
+#' p <- 30
+#' randCor <- 0
+#' randCor3 <- 0
+#' rand_dist <- "norm"
+#' data_str <- "long"
+#' temp.three <- sim_glm_nested3(fixed, random, random3, fixed.param, random.param,
+#' random.param3, cov.param, k, n, p, randCor, randCor3, rand_dist,
+#' data_str = data_str)
+#' head(temp.three)
+#' }
+#' @export 
+sim_glm_nested3 <- function(fixed, random, random3, fixed.param, random.param, random.param3,
+                            cov.param, k, n, p, randCor, randCor3, rand_dist,
+                            data_str, fact.vars = list(NULL),
+                            unbal = FALSE, unbal3 = FALSE, unbalCont = NULL, unbalCont3 = NULL,
+                            ...) {
+  
+  if(randCor > 1 | randCor < -1 | randCor3 > 1 | randCor3 < -1) 
+    stop("Random effect correlation out of range")
+  
+  fixed.vars <- attr(terms(fixed),"term.labels")    ##Extracting fixed effect term labels
+  rand.vars <- attr(terms(random),"term.labels")   ##Extracting random effect term labels
+  rand.vars3 <- attr(terms(random3),"term.labels")   ##Extracting random effect term labels
+  
+  if(length(rand.vars)+1 != length(random.param)) stop("Random lengths not equal")
+  if(length(rand.vars3)+1 != length(random.param3)) stop("Third level random lengths not equal")
+  if({length(fixed.vars)+1} != {length(fixed.param)}) stop("Fixed lengths not equal")
+  
+  if(unbal3 == FALSE) {
+    lvl2ss <- rep(n/k, k)
+    n <- sum(lvl2ss)
+  } else {
+    if(length(unbalCont3) < 2) stop("Must specify unbalCont3 when unbal3 = TRUE")
+    lvl2ss <- round(runif(n = k, min = min(unbalCont3), max = max(unbalCont3)), 0)
+    n <- sum(lvl2ss)
+  }
+  
+  if(unbal == FALSE) {
+    lvl1ss <- rep(p, n)
+    if(is.null(lvl1ss)) stop("lvl1ss is NULL")
+  } else {
+    if(length(unbalCont) < 2) stop("Must specify unbalCont when unbal = TRUE")
+    lvl1ss <- round(runif(n = n, min = min(unbalCont), max = max(unbalCont)), 0)
+  }
+  
+  end <- cumsum(lvl2ss)
+  beg <- c(1, cumsum(lvl2ss) + 1)
+  beg <- beg[-length(beg)]
+  
+  lvl3ss <- sapply(lapply(1:length(beg), function(xx) 
+    lvl1ss[beg[xx]:end[xx]]), sum)
+  
+  rand.eff <- sim_rand_eff(random.param, randCor, n, rand_dist)
+  rand.eff3 <- sim_rand_eff3(random.param3, randCor3, k)
+  
+  Xmat <- sim_fixef_nested3(fixed, fixed.vars, cov.param, k, n = lvl2ss, 
+                            p = lvl1ss, data_str = data_str, fact.vars = fact.vars)
+  
+  reff <- do.call("cbind", lapply(1:ncol(rand.eff), function(xx) 
+    rep(rand.eff[,xx], times = lvl1ss)))
+  colnames(reff) <- c(unlist(lapply(1:ncol(rand.eff), function(xx) paste("b", xx-1, "_2", sep = ""))))
+  
+  reff3 <- do.call("cbind", lapply(1:ncol(rand.eff3), function(xx) 
+    rep(rand.eff3[,xx], times = lvl2ss)))
+  colnames(reff3) <- c(unlist(lapply(1:ncol(rand.eff3), function(xx) paste("b", xx-1, "_3", sep = ""))))
+  
+  Zmat <- model.matrix(random, data.frame(Xmat))
+  Zmat3 <- model.matrix(random3, data.frame(Xmat))
+  
+  sim.data <- data_glm_nested3(Xmat, Zmat, Zmat3, fixed.param, rand.eff, rand.eff3,
+                               k, n = lvl2ss, p = lvl1ss)
+  
+  Xmat <- data.frame(Xmat,reff,sim.data)
+  Xmat$withinID <- unlist(lapply(1:length(lvl1ss), function(xx) 1:lvl1ss[xx]))
+  Xmat$clustID <- rep(1:n, times = lvl1ss)
+  Xmat$clust3ID <- rep(1:k, times = lvl3ss)
+  return(Xmat)
+}
+
+

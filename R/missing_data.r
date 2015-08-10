46644d88b0ff98c3d48d38db9f755d98a0d29302
lebebr01 <lebebr01@gmail.com>
2015-08-10 11:55:22 -0500

    Change name of vignette, rework missing data function.

--- R/missing_data.r
+++ R/missing_data.r
@@ -10,14 +10,17 @@
 #'           the same length as the number of clusters representing the
 #'           percentage of missing data for each cluster
 #' @param type The type of missing data to generate, currently supports
-#'           droput or random missing data.
+#'           droput, random, or missing at random (mar) missing data.
+#' @param miss_cov Covariate that the missing values are based on.
 #' @export 
 missing_data <- function(sim_data, resp_var = 'sim.data',
                          clust_var = 'clustID', miss_prop,
-                         type = c('dropout', 'random')) {
+                         type = c('dropout', 'random', 'mar'),
+                         miss_cov) {
   switch(type,
          dropout = dropout_missing(sim_data, resp_var, clust_var, miss_prop),
-         random = random_missing(sim_data, resp_var, clust_var, miss_prop)
+         random = random_missing(sim_data, resp_var, clust_var, miss_prop),
+         mar = mar_missing(sim_data, resp_var, miss_cov, miss_prop)
          )
 }
 
@@ -104,51 +107,61 @@ dropout_missing <- function(sim_data, resp_var = 'sim.data',
 #' 
 #' @param sim_data Simulated data frame
 #' @param resp_var Response variable to add missing data to
-#' @param clust_var Cluster variable used for the grouping.
 #' @param miss_prop Proportion of missing data overall or a vector
 #'           the same length as the number of clusters representing the
 #'           percentage of missing data for each cluster
+#' @param clust_var Cluster variable used for the grouping.
 #' @export 
-random_missing <- function(sim_data, resp_var = 'sim.data',
-                           clust_var = 'clustID', miss_prop) {
+random_missing <- function(sim_data, resp_var = 'sim.data', miss_prop,
+                           clust_var = NULL) {
   
   if(resp_var %ni% names(sim_data)) {
     stop(paste(resp_var, 'not found in variables of data supplied'))
   }
-  if(clust_var %ni% names(sim_data)) {
-    stop(paste(clust_var, 'not found in variables of data supplied'))
+  if(is.null(clust_var) == FALSE) {
+    if(clust_var %ni% names(sim_data)) {
+      stop(paste(clust_var, 'not found in variables of data supplied'))
+    }
   }
   
-  len_groups <- with(sim_data, tapply(eval(parse(text = resp_var)), 
-                                      eval(parse(text = clust_var)),
-                                      length))
-  
-  num_obs <- nrow(sim_data)
-  
-  if(length(miss_prop) == 1) {
-    if(miss_prop > 1) {
-      miss_prop <- miss_prop / 100
-    }
-    total_missing <- num_obs * miss_prop
-    missing_range <- round((total_missing *.98):(total_missing * 1.02))
-    n_groups <- with(sim_data, length(unique(eval(parse(text = clust_var)))))
-    
-    lim <- prop_limits(miss_prop)
-    
-    num_missing <- 0
-    while(sum(num_missing) %ni% missing_range) {
-      num_missing <- round(len_groups * round(runif(n_groups, lim[1], lim[2]), 2))
-    }
+  if(is.null(clust_var)){
     
   } else {
-    num_missing <- round(len_groups * miss_prop, 0)
+    len_groups <- with(sim_data, tapply(eval(parse(text = resp_var)), 
+                                        eval(parse(text = clust_var)),
+                                        length))
+    
+    num_obs <- nrow(sim_data)
+    
+    if(length(miss_prop) == 1) {
+      if(miss_prop > 1) {
+        miss_prop <- miss_prop / 100
+      }
+      total_missing <- num_obs * miss_prop
+      missing_range <- round((total_missing *.98):(total_missing * 1.02))
+      n_groups <- with(sim_data, length(unique(eval(parse(text = clust_var)))))
+      
+      lim <- prop_limits(miss_prop)
+      
+      num_missing <- 0
+      while(sum(num_missing) %ni% missing_range) {
+        num_missing <- round(len_groups * round(runif(n_groups, lim[1], lim[2]), 2))
+      }
+      
+    } else {
+      num_missing <- round(len_groups * miss_prop, 0)
+    }
+    
+    missing_obs <- lapply(1:length(num_missing), function(xx) 
+      sample(1:len_groups[xx], num_missing[xx]))
+    
+    data_split <- with(sim_data, split(sim_data, eval(parse(text = clust_var))))
+    
   }
   
-  missing_obs <- lapply(1:length(num_missing), function(xx) 
-    sample(1:len_groups[xx], num_missing[xx]))
   
-  data_split <- with(sim_data, split(sim_data, eval(parse(text = clust_var))))
   
+
   sim_data$missing <- do.call("c", lapply(1:length(missing_obs), function(xx)
     ifelse(data_split[[xx]]$withinID %in% missing_obs[[xx]], 1, 0)))
   

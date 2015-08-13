3072378c312392fd5b9d4e80c3197612aec39f4b
lebebr01 <lebebr01@gmail.com>
2015-08-13 12:40:02 -0500

    Adjust sim_pow to reflect changes to sim_reg

--- R/pow_sim.r
+++ R/pow_sim.r
@@ -12,48 +12,59 @@
 #' @param random One sided formula for random effects in the simulation. Must be a subset of fixed.
 #' @param fixed.param Fixed effect parameter values (i.e. beta weights).  Must be same length as fixed.
 #' @param random.param Variance of random effects. Must be same length as random.
-#' @param w.var Number of within cluster variables, including intercept if applicable.   
-#' Also could be number of level one covariates for cross-sectional clustering.
-#' @param cov.param List of mean and variance for fixed effects. Does not include intercept, time, or 
-#' interactions. Must be same order as fixed formula above.
+#' @param cov.param List of mean, sd (standard deviations), and var.type for fixed effects. 
+#'  Does not include intercept, time, factors, or interactions. 
+#'  var.type must be either "lvl1" or "lvl2". Must be same order as fixed formula above.
 #' @param n Cluster sample size.
 #' @param p Within cluster sample size.
 #' @param error_var Scalar of error variance.
 #' @param randCor Correlation between random effects.
-#' @param rand.dist Simulated random effect distribution.  Must be "lap", "chi", "norm", "bimod", 
+#' @param rand_dist Simulated random effect distribution.  Must be "lap", "chi", "norm", "bimod", 
 #' "norm" is default.
 #' @param rand_gen Simulated within cluster error distribution. Must be "lap", "chi", "norm", "bimod", 
 #' "norm" is default.
 #' @param arima TRUE/FALSE flag indicating whether residuals should 
 #'             be correlated. If TRUE, must specify a valid model to pass to 
 #'             arima.sim. See \code{\link{arima.sim}} for examples.
-#' @param data.str Type of data. Must be "cross", "long", or "single".
-#' @param pow.param Number of parameter to calculate power includes intercept where applicable.
+#' @param data_str Type of data. Must be "cross", "long", or "single".
+#' @param fact.vars A nested list of factor, categorical, or ordinal variable specification, 
+#'      each list must include numlevels and var.type (must be "lvl1" or "lvl2");
+#'      optional specifications are: replace, prob, value.labels.
+#' @param unbal A vector of sample sizes for the number of observations for each level 2
+#'  cluster. Must have same length as level two sample size n. Alternative specification
+#'  can be TRUE, which uses additional argument, unbalCont.
+#' @param unbalCont When unbal = TRUE, this specifies the minimum and maximum level one size,
+#'  will be drawn from a random uniform distribution with min and max specified.
+#' @param ... Additional specification needed to pass to the random generating 
+#'             function defined by rand.gen.
+#' @param pow_param Number of parameter to calculate power includes intercept where applicable.
 #' @param alpha What should the per test alpha rate be used for the hypothesis testing.
-#' @param pow.dist Which distribution should be used when testing hypothesis test, z or t?
-#' @param pow.tail One-tailed or two-tailed test?
+#' @param pow_dist Which distribution should be used when testing hypothesis test, z or t?
+#' @param pow_tail One-tailed or two-tailed test?
 #' @export 
-sim_pow_nested <- function(fixed, random, fixed.param, random.param, w.var, cov.param, n, p, error_var, 
-                           randCor, rand.dist, rand_gen, arima = FALSE, data.str,
-                           pow.param, alpha, pow.dist = c("z", "t"), pow.tail = c(1, 2)){
+sim_pow_nested <- function(fixed, random, fixed.param, random.param, cov.param, n, p, 
+                           error_var, randCor, rand_dist, rand_gen, arima,
+                           data_str, fact.vars, unbal, unbalCont, ...,
+                           pow_param, alpha, pow_dist = c("z", "t"), pow_tail = c(1, 2)){
 
-#   temp.nest <- sim_reg_nested(fixed, random, fixed.param, random.param, w.var, cov.param, n, p, 
-#                               error_var, randCor, rand.dist, rand_gen, serCor, serCorVal, data.str)
-#   
-#   fixed.vars <- attr(terms(fixed),"term.labels")    ##Extracting fixed effect term labels
-#   rand.vars <- attr(terms(random),"term.labels")
-#   
-#   fix1 <- paste("sim.data ~", paste(fixed.vars, collapse = "+"))
-#   #ran1 <- paste("(", paste(rand.vars, collapse = "+"), "|clustID)", sep = "")
-#   #fm1 <- as.formula(paste(fix1, ran1, sep = "+ "))
-#   ran1 <- paste("~", paste(rand.vars, collapse = "+"), "|clustID", sep = "")
-#   
-#   #temp.lme <- lme(fixed = as.formula(fix1), data = temp.nest, random = as.formula(ran1))
-#   
-#   crit <- qnorm(alpha/pow.tail, lower.tail = FALSE)
+  temp.nest <- sim_reg_nested(fixed, random, fixed.param, random.param, cov.param, n, p, 
+                              error_var, randCor, rand_dist, rand_gen, arima,
+                              data_str, fact.vars, unbal, unbalCont, ...)
+  
+  fixed.vars <- attr(terms(fixed),"term.labels")    ##Extracting fixed effect term labels
+  rand.vars <- attr(terms(random),"term.labels")
+  
+  fix1 <- paste("sim.data ~", paste(fixed.vars, collapse = "+"))
+  #ran1 <- paste("(", paste(rand.vars, collapse = "+"), "|clustID)", sep = "")
+  #fm1 <- as.formula(paste(fix1, ran1, sep = "+ "))
+  ran1 <- paste("~", paste(rand.vars, collapse = "+"), "|clustID", sep = "")
+  
+  #temp.lme <- lme(fixed = as.formula(fix1), data = temp.nest, random = as.formula(ran1))
+  
+#   crit <- qnorm(alpha/pow_tail, lower.tail = FALSE)
 #   #coefTab <- coef.tbl(temp.lmer)
-#   #testStat <- coefTab[pow.param]
-#   testStat <- summary(temp.lme)$tTable[pow.param, 3]
+#   #testStat <- coefTab[pow_param]
+#   #testStat <- summary(temp.lme)$tTable[pow_param, 3]
 #   
 #   reject <- ifelse(testStat >= crit, 1, 0)
 #   
@@ -79,25 +90,26 @@ sim_pow_nested <- function(fixed, random, fixed.param, random.param, w.var, cov.
 #' @param error_var Scalar of error variance.
 #' @param rand_gen Simulated within cluster error distribution. Must be "lap", "chi", "norm", "bimod", 
 #' "norm" is default.
-#' @param pow.param Number of parameter to calculate power includes intercept where applicable.
+#' @param pow_param Number of parameter to calculate power includes intercept where applicable.
 #' @param alpha What should the per test alpha rate be used for the hypothesis testing.
-#' @param pow.dist Which distribution should be used when testing hypothesis test, z or t?
-#' @param pow.tail One-tailed or two-tailed test?
+#' @param pow_dist Which distribution should be used when testing hypothesis test, z or t?
+#' @param pow_tail One-tailed or two-tailed test?
 #' @export 
 sim_pow_single <- function(fixed, fixed.param, cov.param, n, error_var, rand_gen, 
-                           pow.param, alpha, pow.dist = c("z", "t"), pow.tail = c(1, 2)){
+                           pow_param, alpha, pow_dist = c("z", "t"), pow_tail = c(1, 2)){
   
-  temp.single <- sim.reg.single(fixed, fixed.param, cov.param, n, error_var, rand_gen)
+  temp.single <- sim_reg_single(fixed, fixed.param, cov.param, n, 
+                                error_var, rand_gen)
   fixed.vars <- attr(terms(fixed),"term.labels")
   
   fm1 <- as.formula(paste("sim.data ~", paste(fixed.vars, collapse = "+")))
   
   temp.lm <- lm(fm1, data = temp.single)
   
-  crit <- ifelse(pow.dist == "z", qnorm(alpha/pow.tail, lower.tail = FALSE), 
-                 qt(alpha/pow.tail, df = nrow(temp.single) - length(fixed.param), lower.tail = FALSE))
-  testStat <- ifelse(pow.tail == 2, abs(coefficients(summary(temp.lm))[pow.param, 3]), 
-                     coefficients(summary(temp.lm))[pow.param, 3])
+  crit <- ifelse(pow_dist == "z", qnorm(alpha/pow_tail, lower.tail = FALSE), 
+                 qt(alpha/pow_tail, df = nrow(temp.single) - length(fixed.param), lower.tail = FALSE))
+  testStat <- ifelse(pow_tail == 2, abs(coefficients(summary(temp.lm))[pow_param, 3]), 
+                     coefficients(summary(temp.lm))[pow_param, 3])
   
   reject <- ifelse(testStat >= crit, 1, 0)
   
--- R/sim_pow.r
+++ R/sim_pow.r
@@ -8,50 +8,68 @@
 #' 
 #' @param fixed One sided formula for fixed effects in the simulation.  To suppress intercept add -1 to formula.
 #' @param random One sided formula for random effects in the simulation. Must be a subset of fixed.
+#' @param random3 One sided formula for random effects at third level in the simulation. Must be a subset of fixed
+#'  (and likely of random).
 #' @param fixed.param Fixed effect parameter values (i.e. beta weights).  Must be same length as fixed.
 #' @param random.param Variance of random effects. Must be same length as random.
-#' @param w.var Number of within cluster variables, including intercept if applicable.   
-#' Also could be number of level one covariates for cross-sectional clustering.
+#' @param random.param3 Variance of third level random effects. Must be same length as random3.
 #' @param cov.param List of mean and variance for fixed effects. Does not include intercept, time, or 
 #' interactions. Must be same order as fixed formula above.
+#' @param k Number of third level clusters.
 #' @param n Cluster sample size.
 #' @param p Within cluster sample size.
 #' @param error_var Scalar of error variance.
 #' @param randCor Correlation between random effects.
-#' @param rand.dist Simulated random effect distribution.  Must be "lap", "chi", "norm", "bimod", 
+#' @param randCor3 Correlation between third level random effects.
+#' @param rand_dist Simulated random effect distribution.  Must be "lap", "chi", "norm", "bimod", 
 #' "norm" is default.
-#' @param rand_gen Simulated within cluster error distribution. Must be "lap", "chi", "norm", "bimod", 
-#' "norm" is default.
+#' @param rand_gen Distribution function to pass on to the level one
+#'                  simulation of errors.
 #' @param arima TRUE/FALSE flag indicating whether residuals should 
 #'             be correlated. If TRUE, must specify a valid model to pass to 
 #'             arima.sim. See \code{\link{arima.sim}} for examples.
-#' @param data.str Type of data. Must be "cross", "long", or "single".
-#' @param pow.param Number of parameter to calculate power includes intercept where applicable.
+#' @param data_str Type of data. Must be "cross", "long", or "single".
+#' @param fact.vars A nested list of factor, categorical, or ordinal variable specification, 
+#'      each list must include numlevels and var.type (must be "lvl1" or "lvl2");
+#'      optional specifications are: replace, prob, value.labels.
+#' @param unbal A vector of sample sizes for the number of observations for each level 2
+#'  cluster. Must have same length as level two sample size n. Alternative specification
+#'  can be TRUE, which uses additional argument, unbalCont.
+#' @param unbal3 A vector of sample sizes for the number of observations for each level 3
+#'  cluster. Must have same length as level two sample size k. Alternative specification
+#'  can be TRUE, which uses additional argument, unbalCont3.
+#' @param unbalCont When unbal = TRUE, this specifies the minimum and maximum level one size,
+#'  will be drawn from a random uniform distribution with min and max specified.
+#' @param unbalCont3 When unbal3 = TRUE, this specifies the minimum and maximum level two size,
+#'  will be drawn from a random uniform distribution with min and max specified.
+#'  @param ... Additional parameters passed on to the level one error generating function
+#' @param pow_param Number of parameter to calculate power includes intercept where applicable.
 #' @param alpha What should the per test alpha rate be used for the hypothesis testing.
-#' @param pow.dist Which distribution should be used when testing hypothesis test, z or t?
-#' @param pow.tail One-tailed or two-tailed test?
+#' @param pow_dist Which distribution should be used when testing hypothesis test, z or t?
+#' @param pow_tail One-tailed or two-tailed test?
 #' @param replicates How many replications should be done (i.e. the denominator in power calculation).
-#' @param parallel Whether the computation should be done in parallel, 
-#'  must have parallel backend registered first.  Currently not supported.
 #' @export 
-sim_pow <- function(fixed, random, fixed.param, random.param, w.var, cov.param, n, p, error_var, randCor, 
-                    rand.dist, rand_gen, arima = FALSE, data.str, pow.param, alpha, 
-                    pow.dist = c("z", "t"), pow.tail = c(1, 2), replicates, parallel){
+sim_pow <- function(fixed, random, random3, fixed.param, random.param, random.param3, cov.param, k, n, p, 
+                    error_var, randCor, randCor3, rand_dist, rand_gen, arima = FALSE,
+                    data_str, fact.vars = list(NULL), unbal = FALSE, unbal3 = FALSE, 
+                    unbalCont = NULL, unbalCont3 = NULL,
+                    ..., pow_param, alpha, 
+                    pow_dist = c("z", "t"), pow_tail = c(1, 2), replicates){
   
-  if(data.str == "single"){
+  if(data_str == "single"){
     temp.pow <- replicate(replicates, sim_pow_single(fixed, fixed.param, cov.param, n, error_var,
-                                                     rand_gen, arima, pow.param, alpha, pow.dist, pow.tail))
+                                                     rand_gen, arima, pow_param, alpha, pow_dist, pow_tail))
     
     #nbatch <- 3
     #system.time(temp.pow <- foreach(i = 1:(9999/nbatch), .combine = "c", .packages = "simReg") %dopar% {
      # replicate(nbatch, sim.pow.single(fixed, fixed.param, cov.param, n, error_var,
-      #               rand_gen, pow.param, alpha, pow.dist, pow.tail))
+      #               rand_gen, pow_param, alpha, pow_dist, pow_tail))
     #})
   } else {
-    temp.pow <- replicate(replicates, sim_pow_nested(fixed, random, fixed.param, random.param, w.var, 
-                                                     cov.param, n, p, error_var, randCor, rand.dist, rand_gen,
-                                                     arima, data.str, pow.param, alpha, 
-                                                     pow.dist, pow.tail))
+    temp.pow <- replicate(replicates, sim_pow_nested(fixed, random, fixed.param, random.param, cov.param, n, p, 
+                                                     error_var, randCor, rand_dist, rand_gen, arima,
+                                                     data_str, fact.vars, unbal, unbalCont, ..., pow_param, alpha, 
+                                                     pow_dist, pow_tail))
   }
   
   powerMat <- data.frame(cbind(matrix(table(temp.pow), ncol = 2, nrow = 1), table(temp.pow)[2]/length(temp.pow)))

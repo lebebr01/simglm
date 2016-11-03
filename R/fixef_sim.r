1f8e22a37bc432cf7be620d9ec6ed70216200ef9
lebebr01 <lebebr01@gmail.com>
2016-11-02 20:35:28 -0500

    Adding simulation dummy code to 3 level and single level

--- R/fixef_sim.r
+++ R/fixef_sim.r
@@ -239,6 +239,9 @@ sim_fixef_nested3 <- function(fixed, fixed_vars, cov_param, k, n, p, data_str,
     int.loc <- grep(":", fixed_vars)
     colnames(Xmat) <- fixed_vars[-int.loc]
   } 
+  if(any(grepl("\\.f|\\.o|\\.c", fixed_vars, ignore.case = TRUE))) {
+    fixed <- search_factors(fixed_vars)
+  }
   Xmat <- model.matrix(fixed, data.frame(Xmat))
   Xmat
 }
@@ -329,6 +332,9 @@ sim_fixef_single <- function(fixed, fixed_vars, n, cov_param, cor_vars = NULL,
     int.loc <- grep(":", fixed_vars)
     colnames(Xmat) <- fixed_vars[-int.loc]
   } 
+  if(any(grepl("\\.f|\\.o|\\.c", fixed_vars, ignore.case = TRUE))) {
+    fixed <- search_factors(fixed_vars)
+  }
   Xmat <- model.matrix(fixed, data.frame(Xmat))
   Xmat
 }

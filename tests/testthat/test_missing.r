5d019d544137a570d563c6a01a0ba5799f94f61a
lebebr01 <lebebr01@gmail.com>
2016-11-22 14:21:22 -0600

    Add missing data tests.

---
+++ tests/testthat/test_missing.r
@@ -0,0 +1,65 @@
+context("missing data")
+
+test_that("dropout missing occurring", {
+  # Simulate longitudinal data
+  fixed <- ~1 + time + diff + act + time:act
+  random <- ~1 + time + diff
+  fixed_param <- c(4, 2, 6, 2.3, 7)
+  random_param <- list(random_var = c(7, 4, 2), rand_gen = "rnorm")
+  cov_param <- list(mean = c(0, 0), sd = c(1.5, 4), var_type = c("lvl1", "lvl2"))
+  n <- 150
+  p <- 30
+  error_var <- 4
+  with_err_gen <- 'rnorm'
+  data_str <- "long"
+  temp_long <- sim_reg(fixed, random, random3 = NULL, fixed_param, random_param, random_param3 = NULL,
+                       cov_param, k = NULL, n, p, error_var, with_err_gen, data_str = data_str)
+  
+  # simulate missing data
+  temp_long_miss <- missing_data(temp_long, miss_prop = .25, type = 'dropout', 
+                                 clust_var = 'clustID', within_id = 'withinID')
+  
+  expect_equal(prop.table(table(temp_long_miss$missing))[2], .25, 
+               tolerance = .05)
+  
+  temp_long_miss <- missing_data(temp_long, miss_prop = .05, type = 'dropout', 
+                                 clust_var = 'clustID', within_id = 'withinID')
+  
+  expect_equal(prop.table(table(temp_long_miss$missing))[2], .05, 
+               tolerance = .05)
+})
+
+test_that("MAR missing", {
+  # simulate data
+  fixed <- ~1 + age + income
+  fixed_param <- c(2, 0.3, 1.3)
+  cov_param <- list(mean = c(0, 0), sd = c(4, 3), 
+                    var_type = c("single", "single"))
+  n <- 4500
+  error_var <- 3
+  with_err_gen <- 'rnorm'
+  temp_single <- sim_reg(fixed = fixed, fixed_param = fixed_param,
+                         cov_param = cov_param,
+                         n = n, error_var = error_var, with_err_gen = with_err_gen,
+                         data_str = "single")
+  
+  # generate missing data
+  miss_prop <- c(0.5, 0.45, 0.4, 0.35, 0.3, 0.25, 0.2, 0.15, 0.1, 0.05)
+  miss_prop <- rep(miss_prop, each = 450)
+  tmp_single_miss <- missing_data(temp_single, miss_prop = miss_prop, 
+                                  type = 'mar', miss_cov = 'income')
+  
+  diff <- sum(abs(prop.table(table(tmp_single_miss$miss_prop, 
+                   is.na(tmp_single_miss$sim_data2)), 1)[,2] 
+  - unique(sort(miss_prop))))
+  
+  expect_equal(diff, .1, tolerance = .1)
+})
+
+test_that("mcar missing", {
+  tmp_single_miss <- missing_data(temp_single, miss_prop = .25, 
+                                  type = 'random', clust_var = NULL)
+  expect_equal(prop.table(table(is.na(tmp_single_miss$sim_data2)))[2], 
+               .25, tolerance = .05)
+})
+

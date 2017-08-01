485903d3372d2dc7aad3093b45dd5442ff39eb66
Brandon LeBeau <lebebr01@gmail.com>
2017-08-01 11:11:55 -0500

    Add fixef_sim probs fix to nested

--- R/fixef_sim.r
+++ R/fixef_sim.r
@@ -398,8 +398,15 @@ sim_fixef_single <- function(fixed, fixed_vars, n, cov_param, cor_vars = NULL,
       probs <- fact_vars$prob
       fact_vars$prob <- NULL
     }
+    if(is.null(fact_vars$replace)) {
+      replace_f <- NULL
+    } else {
+      replace_f <- fact_vars$replace
+      fact_vars$replace <- NULL
+    }
     Xmat <- cbind(Xmat,  do.call(cbind, purrr::pmap(fact_vars, sim_factor,
-                                                    prob = probs)))
+                                                    prob = probs,
+                                                    replace = replace_f)))
   }
   
   if(n.int == 0){

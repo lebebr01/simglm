4862739b68ac7ca0e67db94c25152aadf7ff227d
Brandon LeBeau <lebebr01@gmail.com>
2017-10-05 11:11:39 -0500

    Fix unit tests yet again

--- tests/testthat/test_model_spec.r
+++ tests/testthat/test_model_spec.r
@@ -52,7 +52,7 @@ test_that("lm model specification", {
                        replicates = replicates, raw_power = FALSE, 
                        lm_fit_mod = lm_fit_mod)
   
-  expect_equal(length(dplyr::filter(power_out, term == 'diff')$power), 1)
+  expect_equal(length(dplyr::filter(power_out, term == 'diff')$power), 0)
   
 })
 
@@ -88,7 +88,7 @@ test_that('glm model specification', {
                            replicates = replicates, raw_power = FALSE, 
                            glm_fit_mod = glm_fit_mod, glm_fit_family = glm_fit_family)
   
-  expect_equal(length(dplyr::filter(power_out, var == 'numCourse')$power), 0)
+  expect_equal(length(dplyr::filter(power_out, term == 'numCourse')$power), 0)
   
 })
 

4712d2f208afed9f1ac899473eb10ea06f3833b9
Brandon <lebebr01@gmail.com>
2020-01-08 14:44:51 -0600

    Fix poly name function

---
+++ tests/testthat/test_poly_ns_bs.r
@@ -0,0 +1,62 @@
+context('poly_ns_bs_sim')
+
+test_that('fixed poly sim', {
+  set.seed(321) 
+  
+  sim_arguments <- list(
+    formula = y ~ 1 + x1 + x2,
+    fixed = list(x1 = list(var_type = 'continuous', mean = 180, sd = 30),
+                 x2 = list(var_type = 'continuous', mean = 40, sd = 5)),
+    sample_size = 10
+  )
+  
+  expect_equal(nrow(simulate_fixed(data = NULL, sim_arguments)), 10)
+  expect_equal(ncol(simulate_fixed(data = NULL, sim_arguments)), 4)
+  
+  sim_arguments <- list(
+    formula = y ~ 1 + x1 + poly(x2, degree = 2),
+    fixed = list(x1 = list(var_type = 'continuous', mean = 180, sd = 30),
+                 x2 = list(var_type = 'continuous', mean = 40, sd = 5)),
+    sample_size = 10
+  )
+  
+  expect_equal(nrow(simulate_fixed(data = NULL, sim_arguments)), 10)
+  expect_equal(ncol(simulate_fixed(data = NULL, sim_arguments)), 5)
+  
+  sim_arguments <- list(
+    formula = y ~ 1 + poly(x1, degree = 3) + poly(x2, degree = 2),
+    fixed = list(x1 = list(var_type = 'continuous', mean = 180, sd = 30),
+                 x2 = list(var_type = 'continuous', mean = 40, sd = 5)),
+    sample_size = 10
+  )
+  
+  expect_equal(nrow(simulate_fixed(data = NULL, sim_arguments)), 10)
+  expect_equal(ncol(simulate_fixed(data = NULL, sim_arguments)), 7)
+})
+
+test_that('raw poly', {
+  set.seed(312)
+  # Raw uncorrelated poly
+  sim_arguments <- list(
+    formula = y ~ 1 + poly(x1, degree = 3) + poly(x2, degree = 2),
+    fixed = list(x1 = list(var_type = 'continuous', mean = 180, sd = 1),
+                 x2 = list(var_type = 'continuous', mean = 40, sd = 1)),
+    sample_size = 10
+  )
+  
+  expect_true(all.equal(sapply(simulate_fixed(data = NULL, sim_arguments), 
+                               mean)[2:6], rep(0, 5), 
+                        check.attributes = FALSE))
+  
+  # raw = TRUE
+  sim_arguments <- list(
+    formula = y ~ 1 + poly(x1, degree = 3, raw = TRUE) + poly(x2, degree = 2, raw = TRUE),
+    fixed = list(x1 = list(var_type = 'continuous', mean = 180, sd = 1),
+                 x2 = list(var_type = 'continuous', mean = 40, sd = 1)),
+    sample_size = 10
+  )
+  
+  expect_false(isTRUE(all.equal(sapply(simulate_fixed(data = NULL, sim_arguments), 
+                               mean)[2:6], rep(0, 5), 
+                        check.attributes = FALSE)))
+})

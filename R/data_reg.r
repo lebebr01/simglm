ce329bf2e7b500a8681f95bd02b3bc37a2892cfb
Brandon <lebebr01@gmail.com>
2022-12-07 12:19:13 -0600

    #97 generalize for ordinal sim

--- R/data_reg.r
+++ R/data_reg.r
@@ -62,10 +62,11 @@ generate_response <- function(data, sim_args, keep_intermediate = TRUE, ...) {
   }
   
   if(is.list(sim_args[['reg_weights']])) {
-    fixed_outcome <- do.call("cbind", 
+    fixed_outcome <- data.frame(do.call("cbind", 
               lapply(seq_along(sim_args[['reg_weights']]), 
                      function(xx) 
-                       as.matrix(Xmat) %*% sim_args[['reg_weights']][[xx]]))
+                       as.matrix(Xmat) %*% sim_args[['reg_weights']][[xx]])))
+    names(fixed_outcome) <- paste0('logit', 1:ncol(fixed_outcome))
   } else {
     fixed_outcome <- as.matrix(Xmat) %*% sim_args[['reg_weights']]
   }
@@ -97,10 +98,18 @@ generate_response <- function(data, sim_args, keep_intermediate = TRUE, ...) {
   }
   
   if(keep_intermediate) {
-    response_outcomes <- data.frame(
-      fixed_outcome = fixed_outcome,
-      random_effects = random_effects
-    )
+    if(is.list(sim_args[['reg_weights']])) {
+      response_outcomes <- data.frame(
+        fixed_outcome,
+        random_effects = random_effects
+      )
+    } else {
+      response_outcomes <- data.frame(
+        fixed_outcome = fixed_outcome,
+        random_effects = random_effects
+      )
+    }
+    
     data <- cbind(data, response_outcomes, row.names = NULL)
   }
   
@@ -108,11 +117,15 @@ generate_response <- function(data, sim_args, keep_intermediate = TRUE, ...) {
     data['error'] <- 0
   }
   
-  outcome <- as.numeric(unlist(fixed_outcome + random_effects + data['error']))
+  outcome <- fixed_outcome + random_effects + data[['error']]
   
   if(!is.null(sim_args[['outcome_type']])){
     trans_outcome <- transform_outcome(outcome, type = sim_args[['outcome_type']])
-    data <- cbind(data, untransformed_outcome = outcome)
+    if(ncol(outcome) > 1) {
+      data <- cbind(data, outcome) 
+    } else {
+      data <- cbind(data, untransformed_outcome = outcome)
+    }
     data[outcome_name] <- trans_outcome
   } else {
     data[outcome_name] <- outcome

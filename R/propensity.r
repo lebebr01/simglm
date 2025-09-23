737e7c3323e4f7fa18f2a917dda5c07a1a29e9b8
Brandon LeBeau <lebebr01@gmail.com>
2025-09-23 11:54:12 -0500

    remove fit_propensity function

--- R/propensity.r
+++ R/propensity.r
@@ -19,27 +19,6 @@ simulate_propensity <- function(sim_args) {
   simglm(sim_args[['propensity']])
 }
 
-#' Function to fit initial propensity score model
-#'
-#' After simulation the logistic regression propensity score model.
-#'
-#' @param data Simulated data to pass to the function.
-#' @param sim_args A named list with special model formula syntax. See details and examples
-#'   for more information. The named list may contain the following:
-#'   \itemize{
-#'     \item fixed: This is the fixed portion of the model (i.e. covariates)
-#'     \item random: This is the random portion of the model (i.e. random effects)
-#'     \item error: This is the error (i.e. residual term).
-#'   }
-#' @export
-fit_propensity <- function(data, sim_args) {
-  # switch(
-  #   var_type,
-  #   covariate = fit_propensity_covariate(data = data, sim_args = sim_args)
-  #   ipw = fit_propensity_ipw(data = data, sim_args = sim_args)
-  # )
-}
-
 extract_propensity <- function(data, sim_args) {
   if (is.null(sim_args[['propensity_model']][['formula']])) {
     if (is.null(sim_args[['propensity']][['formula']])) {
